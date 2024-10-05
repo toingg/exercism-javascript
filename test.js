162fe8c1c79d57dc233e3cb23812e052f47b11cb
Fachrizal Ardiansyah <fach.ardiansyah@gmail.com>
2024-10-05 17:56:44 +0700

    Concept completed: Strings. Exercise completed: Poetry Club Door Policy

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-console.log(230/22)
+// console.log(230/22)
+
+// const setering = "test";
+
+// console.log(setering[2]);
+// console.log(setering.length);
+
+// const penggabungan = "iya"
+
+// console.log(setering + " " + penggabungan)
+
+function backDoorPassword(word) {
+  return word.slice(0, 1).toUpperCase + word.slice(1) + ", please";
+}
+
+console.log(backDoorPassword("test"));
