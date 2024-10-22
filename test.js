f0483666a99f385c7fb23c14ce5c8b4db12a3f31
toingg <fach.ardiansyah@gmail.com>
2024-10-22 19:58:13 +0700

    Concept completed: Closures. Exercise completed: Coordinate Transformation

--- test.js
+++ test.js
@@ -226,5 +226,19 @@ function limesToCut(wedgesNeeded, limes) {
 // };
 // scaleRecipe(recipe2, 2)\
 
-const tes = "Happy Birthday!"
-console.log(tes.length);
+// const tes = "Happy Birthday!"
+// console.log(tes.length);
+
+let counter = 0;
+
+// This function closure increments the counter's state in the outer lexical context.
+// This way the counter can be shared between many calling contexts.
+function increment() {
+
+  counter += 1;
+  return counter;
+}
+
+console.log(increment());
+
+
